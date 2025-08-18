6a06779ea64ddf5d8a778bb206420ab7ca9e66a8
rahulvarshney <rahulvarshney@gmail.com>
2025-08-18 07:16:31 -0700

    0.0.12

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
 function handler(_req: Request): Promise<Response> {
     let url = new URL(_req.url);
     let email = url.search;
-    console.log ("0.0.11");
+    console.log ("0.0.12");
     let body = "from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=0.0.10&text=hardcoded";
-    console.log(email);
-    console.log(body);
+    console.log("email var is " + email);
+    console.log("body var is " + body);
     const postRequest = new Request("https://api.forwardemail.net/v1/emails", {
 //        method: "POST",
         method: "GET",
