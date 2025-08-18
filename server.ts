87e04f4fa98102fdf6d544723bc3ad481f75372f
rahulvarshney <rahulvarshney@gmail.com>
2025-08-17 17:15:54 -0700

    url.search

--- server.ts
+++ server.ts
@@ -1,12 +1,32 @@
-function handler(_req: Request): Response {
-    let searchParams = new URLSearchParams(_req.url);
+function handler(_req: Request): Promise<Response> {
     let url = new URL(_req.url);
+  
+  
+    const postRequest = new Request("https://api.forwardemail.net/v1/emails", {
+        method: "POST",
+//        method: "GET",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic OWRhOTZjZmI5OGU1ZWIxOWMwYjA0ODUxOg=="
+
+        },
+        body: url.search,
+    });
+
+    // Make the POST request and return its response
+    return fetch(postRequest);
+}
+
+
+    let searchParams = new URLSearchParams(_req.url);
     let params = new URLSearchParams(url.search);
     console.log(url.search);
     console.log(params);
     console.log(searchParams.size)
 //    console.log(searchParams[0]);
 //        console.log(JSON.stringify(searchParams.entries));
+
+
 
     return new Response(url.search);
 
