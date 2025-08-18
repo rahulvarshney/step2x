5549e6b3829cd90c82fa90ae52c671f54012ac09
rahulvarshney <rahulvarshney@gmail.com>
2025-08-17 20:49:36 -0700

    0.0.11

--- server.ts
+++ server.ts
@@ -1,19 +1,19 @@
 function handler(_req: Request): Promise<Response> {
     let url = new URL(_req.url);
     let email = url.search;
-    console.log ("0.0.9");
-    let body = "from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=0.0.9&text=hardcoded";
+    console.log ("0.0.11");
+    let body = "from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=0.0.10&text=hardcoded";
     console.log(email);
     console.log(body);
     const postRequest = new Request("https://api.forwardemail.net/v1/emails", {
-        method: "POST",
-//        method: "GET",
+//        method: "POST",
+        method: "GET",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": "Basic OWRhOTZjZmI5OGU1ZWIxOWMwYjA0ODUxOg=="
 
         },
-        body: body,
+//        body: body,
     });
 
     // Make the POST request and return its response
